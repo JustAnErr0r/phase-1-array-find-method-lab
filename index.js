c3fd797a7ce8e75998fc1d302c369dd9fd662d85
Madeleine Heineman <mheineman16@gmail.com>
2022-09-05 22:41:46 -0400

    commit tree 4bb5343ddd8ea978e26c03de044961fbaa2ed474

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+//function = superbowlwin
+// will find win condition
+//will return year of win condition
+
+//function revieves 1 arguement = array of objects
+//object with two properties = year/result
+//returns year when win
+//if no win = undefined default?
+
+function superbowlWin(smth){
+    return smth.find(callBckFn).year
+}
+
+function callBckFn(element){
+    //uses find to see if any obj is a "W"
+    //use find() method
+    if (element.result === "W") {
+        return true;
+    } else {
+        return false;
+    }
+  }
+
+const year = superbowlWin(record)
